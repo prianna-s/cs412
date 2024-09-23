ed89524a961e2e9586a1abaa103f560ba5f742ae
Prianna Sharan <psharan@bu.edu>
2024-09-23 16:35:12 -0400

    Allows hosts

--- cs412/settings.py
+++ cs412/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-130$k4$+wa@m9s^cjp^k&dut_hbg)o29txi(!#ekegr8r@nl5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://susan-sontag-app-2d5b6f87411e.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'quotes/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
